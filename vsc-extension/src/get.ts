a3199050fac9395ab25a6cbf92bb2a28c05d141e
yudai <sugi.nut@gmail.com>
2023-11-26 23:15:36 +0100

    s3からobjectURLの取得処理追加

--- vsc-extension/src/get.ts
+++ vsc-extension/src/get.ts
@@ -1,7 +1,15 @@
 import * as vscode from 'vscode';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+dotenv.config({ path: path.join(__dirname, '.env') });
+console.log(path.join(__dirname, '.env'));
+
+const bucketName = process.env.bucket_name;
+const region = process.env.region;
 
 export function get() {
     // グラフURLを取得
-    const graphUrl = "https://your-s3-graph.png";
+    const graphUrl = `http://${bucketName}.s3.${region}.amazonaws.com/graph.png`;
     vscode.window.showInformationMessage(graphUrl);
 }
