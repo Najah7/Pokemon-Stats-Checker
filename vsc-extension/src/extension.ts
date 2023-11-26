f63c7c3864bbb08a99cbff4c46499e8db8153c15
yudai <sugi.nut@gmail.com>
2023-11-27 00:46:29 +0100

    ポケモン探索処理追加

--- vsc-extension/src/extension.ts
+++ vsc-extension/src/extension.ts
@@ -2,9 +2,9 @@ import * as vscode from 'vscode';
 import * as diff from 'diff';
 import { button } from './button';
 import { f } from './function';
-import { start, startTime } from './start';
+import { start, startTime, trueAnswer } from './start';
 import { post } from './post';
-import { get } from './get';
+import { getUrl, getPokemon } from './get';
 import { auth, userName } from './auth';
 
 export function activate(context: vscode.ExtensionContext) {
@@ -18,10 +18,9 @@ export function activate(context: vscode.ExtensionContext) {
 	var previousText: string = "";
 	var typeCount: number = 0;
 	var typoCount: number = 0;
-	var CCount: number = 0;
-	var DCount: number = 0;
+	var specialAttackCount: number = 0;
+	var specialDiffenceCount: number = 0;
 	var currentTextLength: number = 0;
-	const trueAnswer = "3";
 
 	// ステータスバーにボタンを表示
 	context.subscriptions.push(button);
@@ -38,55 +37,60 @@ export function activate(context: vscode.ExtensionContext) {
 			title: '答えを半角で入力（面積が3の場合: 3）'
 		});
 
+		// 正解の場合
 		if (answer === trueAnswer && !isNaN(startTime)) {
-			// 正解の場合、経過時間を計算
+			// 経過時間を計算
 			const elapsedTime = (Date.now() - startTime) / 1000; // ミリ秒から秒に変換
 
 			// H: HP(冗長さ、文字数)
-			const H: number = f(300 - currentTextLength);
+			const h: number = f(300 - currentTextLength);
 
 			// A: こうげき（タイピング率=キータイプ回数/経過時間)
 			const typingRate = typeCount / elapsedTime * 100;
-			const A: number = f(typingRate);
+			const a: number = f(typingRate);
 
 			// B: ぼうぎょ(タイポ率=1文字削除回数/経過時間)
 			const typoRate = typoCount / elapsedTime * 100;
-			const B: number = f(110 - typoRate);
+			const b: number = f(110 - typoRate);
 
 			// C: とくこう(ペースト、インラインサジェスト)
-			const CRate = CCount / elapsedTime * 100;
-			const C: number = f(CRate * 10);
+			const specialAttackRate = specialAttackCount / elapsedTime * 100;
+			const c: number = f(specialAttackRate * 10);
 
 			// D: とくぼう(選択範囲削除)
-			const DRate = DCount / elapsedTime * 100;
-			const D: number = f(100 - DRate);
+			const specialDiffenceRate = specialDiffenceCount / elapsedTime * 100;
+			const d: number = f(100 - specialDiffenceRate);
 
 			// S: すばやさ(完成までの時間)
-			const S: number = f(160 - elapsedTime);
+			const s: number = f(160 - elapsedTime);
+
+			// 種族値特徴の近いポケモンを探索
+			getPokemon(h, a, b, c, d, s);
 
 			// 種族値の計算
-			const tribeValue: number = H + A + B + C + D + S;
+			const baseStats: number = h + a + b + c + d + s;
 
 			// メッセージとして出力
-			vscode.window.showInformationMessage(`H${H} A${A} B${B} C${C} D${D} S${S}`);
-			vscode.window.showInformationMessage(`あなたの種族値は${tribeValue}!`);
+			vscode.window.showInformationMessage(`H${h} A${a} B${b} C${c} D${d} S${s}`);
+			vscode.window.showInformationMessage(`あなたの種族値は${baseStats}!`);
 
 			// メトリクスをPOST
-			post(H, A, B, C, D, S);
+			post(h, a, b, c, d, s);
 
+		// 不正解の場合
 		} else if (answer !== trueAnswer) {
-			// 不正解の場合
 			vscode.window.showInformationMessage(`答えが違います`);
+
+		// startTimeがNaNの場合
 		} else if (isNaN(startTime)) {
-			// startTimeがNaNの場合
 			vscode.window.showInformationMessage(`pentagon:startコマンドで問題を開始してください`);
 		}
 	});
 
 	// メトリクス取得のためのエディター状態取得処理
 	vscode.window.onDidChangeTextEditorSelection(event => {
+		// 問題を開始している場合
 		if (!isNaN(startTime)) {
-			// 問題を開始している場合
 			const { textEditor } = event;
 
 			// 現在のテキスト
@@ -106,7 +110,7 @@ export function activate(context: vscode.ExtensionContext) {
 						console.log("A", part.value);
 					} else {
 						// 文字数が1より大きい場合の処理
-						CCount++;
+						specialAttackCount++;
 						console.log("C", part.value);
 					}
 				}
@@ -118,7 +122,7 @@ export function activate(context: vscode.ExtensionContext) {
 						console.log("B", part.value);
 					} else {
 						// 文字数が1より大きい場合の処理
-						DCount++;
+						specialDiffenceCount++;
 						console.log("D", part.value);
 					}
 				}
@@ -131,7 +135,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// >pentagon:get （グラフURLの取得)
 	disposable = vscode.commands.registerCommand('pentagon.get', () => {
-		get();
+		getUrl();
 	});
 
 	// コマンド実行時にdisposableの処理を行う
