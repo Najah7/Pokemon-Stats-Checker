e06485fb9a7b655451327dee7c521af4a3dfa59a
Najah7 <najahmatsuo7@gmail.com>
2023-11-28 05:15:14 +0900

    [Add] reader

---
+++ utils/db/reader.ts
@@ -0,0 +1,56 @@
+const fs = require('fs');
+const axios = require('axios');
+const path = require('path');
+
+import { read } from 'fs';
+import { Pokemon } from '../../types/pokemons';
+
+// HACK: スマートな方法があれば修正
+const PROJECT_ROOT = path.join(__dirname, '..', '..'); // プロジェクトのルートディレクトリ
+const DB_PATH = path.join(PROJECT_ROOT, 'data', 'pokemons.json'); // ポケモンのデータベースのパス
+// HACK: envとかで管理したい
+const MAX_POKEMON_ID = 151; // 第1世代のポケモンの数が151匹
+
+let pokemonList: Pokemon[] = [];
+
+const readPokemonDataFromJson = (): Pokemon[] => {
+    if (pokemonList.length === 0) {
+        const data = fs.readFileSync(DB_PATH, 'utf8');
+        pokemonList = JSON.parse(data);
+    }
+    return pokemonList;
+}
+
+const readPokemons = (): Pokemon[] => {
+    readPokemonDataFromJson();
+    return pokemonList;
+}
+
+const readPokemonById = (id: number): Pokemon => {
+    if (pokemonList.length === 0) {
+        readPokemonDataFromJson();
+    }
+    const pokemon = pokemonList.find((pokemon: Pokemon) => pokemon.id === id);
+    if (!pokemon) {
+        console.error(`[error] No pokemon found with id: ${id}`);
+        console.info(`[hint] We support only 1st generation pokemon between id 1 and ${MAX_POKEMON_ID}`);
+        throw new Error(`No pokemon found with id: ${id}`);
+    }
+    return pokemon;
+}
+
+const readPokemonByName = (name: string): Pokemon => {
+    if (pokemonList.length === 0) {
+        readPokemonDataFromJson();
+    }
+    const pokemon = pokemonList.find((pokemon: Pokemon) => pokemon.name === name);
+    if (!pokemon) {
+        throw new Error(`No pokemon found with name: ${name}`);
+    }
+    return pokemon;
+}
+
+export { readPokemons, readPokemonById, readPokemonByName };
+
+
+
