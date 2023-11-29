7dafdef4682e88d5342ede6c1ed5831221e72a48
Najah7 <najahmatsuo7@gmail.com>
2023-11-30 08:57:41 +0900

    [Add] sample code for sharing codes to use env for team members

---
+++ vsc-extension/src/utils/db/sample.ts
@@ -0,0 +1,34 @@
+// TODO: Remove this file after sharing the code with team members.
+import axios, { AxiosRequestConfig } from 'axios';
+import * as env from './config';
+
+
+const data = JSON.stringify({
+  "collection": env.POKEMON_COLLECTION,
+  "database": env.DB,
+  "dataSource": env.DATA_SOURCE,
+  "projection": {
+    "id": env.INCLUDE,
+    "name": env.INCLUDE,
+    "stats": env.INCLUDE
+  },
+});
+
+const config: AxiosRequestConfig = {
+    method: 'post',
+    url: `${env.DB_ENDPOINT}/action/findOne`,
+    headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Request-Headers': '*',
+        'api-key': env.DB_API_KEY,
+    },
+    data: data
+};
+
+axios(config)
+    .then(function (response) {
+        console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
