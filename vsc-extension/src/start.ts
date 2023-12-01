1c7d4ab969f1527357a43247319a06930791433a
umaidashi <umaidashi18@gmail.com>
2023-12-01 15:47:08 +0900

    delete: past question text

--- vsc-extension/src/start.ts
+++ vsc-extension/src/start.ts
@@ -1,7 +1,6 @@
 import * as vscode from "vscode";
 
 export let startTime: number;
-export const trueAnswer = "3";
 
 // TODO: どうにかする
 const QUIESTIONS = [
@@ -12,7 +11,6 @@ const QUIESTIONS = [
 ];
 
 export function start() {
-  vscode.window.showInformationMessage(`低辺2m、高さ3mの3角形の面積は?`);
   startTime = Date.now();
   const question = QUIESTIONS[0];
   return { question };
