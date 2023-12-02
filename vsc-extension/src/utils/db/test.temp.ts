f79d54a545fbd5c6c85c1d7e4a53243e201cf7b5
Najah7 <najahmatsuo7@gmail.com>
2023-12-02 22:37:09 +0900

    [Add&Remove] tests

--- vsc-extension/src/utils/db/test.temp.ts
+++ vsc-extension/src/utils/db/test.temp.ts
@@ -5,9 +5,9 @@ import { Question } from "../../types/question";
 import { Score } from "../../types/score";
 
 import { readPokemons, readPokemonById, readPokemonByName } from "./pokemons/reader";
-import { readQuestions, readQuestionById } from "./questions/reader";
+import { readQuestions } from "./questions/reader";
 
-import { readScores, readScoreByUserName } from "./scores/reader";
+import { readScoreByUserName } from "./scores/reader";
 import { insertScore, updateScore, deleteScore } from "./scores/writer";
 
 const DISPLAY_ALL = false;
@@ -21,67 +21,60 @@ const run = async (): Promise<void> => {
     if (DISPLAY_ALL) console.log(pokemons);
 
     const p: Pokemon = await readPokemonById(1);
+    console.log(p)
     console.log('fetch pokemon by id');
     console.log(
-        p.id === 1 &&
+        p.pokemonId === 1 &&
         p.name === "フシギダネ" &&
-        p.stats.hp === 45 &&
-        p.stats.attack === 49 &&
-        p.stats.defense === 49 &&
-        p.stats["special-attack"] === 65 &&
-        p.stats["special-defense"] === 65 &&
-        p.stats.speed === 45 ? "success" : "failed"
+        p.baseStats.hp === 45 &&
+        p.baseStats.attack === 49 &&
+        p.baseStats.defense === 49 &&
+        p.baseStats["specialAttack"] === 65 &&
+        p.baseStats["specialDefense"] === 65 &&
+        p.baseStats.speed === 45 ? "success" : "failed"
         );
     if (DISPLAY_SINGLE) console.log(p);
 
     const p2: Pokemon = await readPokemonByName("フシギダネ");
     console.log('fetch pokemon by name');
     console.log(
-        p2.id === 1 &&
+        p2.pokemonId === 1 &&
         p2.name === "フシギダネ" &&
-        p2.stats.hp === 45 &&
-        p2.stats.attack === 49 &&
-        p2.stats.defense === 49 &&
-        p2.stats["special-attack"] === 65 &&
-        p2.stats["special-defense"] === 65 &&
-        p2.stats.speed === 45 ? "success" : "failed"
+        p2.baseStats.hp === 45 &&
+        p2.baseStats.attack === 49 &&
+        p2.baseStats.defense === 49 &&
+        p2.baseStats["specialAttack"] === 65 &&
+        p2.baseStats["specialDefense"] === 65 &&
+        p2.baseStats.speed === 45 ? "success" : "failed"
         );
     if (DISPLAY_SINGLE) console.log(p2);
 
     const questions = await readQuestions();
     console.log('fetch all questions');
-    console.log(questions.size !== 0 ? "success" : "failed");
+    console.log(questions.length !== 0 ? "success" : "failed");
     if (DISPLAY_ALL) console.log(questions);
 
-    const q: Question = await readQuestionById(1);
-    console.log('fetch question by id');
-    console.log(
-        q.id === 1 &&
-        q.text === "What is the capital of India?" &&
-        q.answer === "New Delhi" ? "success" : "failed"
-        );
-    if (DISPLAY_SINGLE) console.log(q);
 
     const s: Score = {
-        name: "test",
-        stats: {
+        userName: "test",
+        baseStats: {
             hp: 100,
             attack: 100,
             defense: 100,
-            "special-attack": 100,
-            "special-defense": 100,
+            "specialAttack": 100,
+            "specialDefense": 100,
             speed: 100
         }
     };
 
     const s2: Score = {
-        name: "test",
-        stats: {
+        userName: "test",
+        baseStats: {
             hp: 0,
             attack: 0,
             defense: 0,
-            "special-attack": 0,
-            "special-defense": 0,
+            "specialAttack": 0,
+            "specialDefense": 0,
             speed: 0
         }
     };
@@ -93,9 +86,8 @@ const run = async (): Promise<void> => {
     if (DISPLAY_SINGLE) console.log(score);
     deleteScore("test");
 
-    const scores = await readScores();
-    console.log(scores.size !== 0 ? "success" : "failed");
-    if (DISPLAY_ALL) console.log(scores);
+    const qs: Question[] = await readQuestions();
+    console.log(qs);
 }
 
 run();
