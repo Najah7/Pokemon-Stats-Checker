b0a4837d3a545c3ed923a949a9e292bfc4e2b25b
umaidashi <umaidashi18@gmail.com>
2023-12-02 10:10:22 +0900

    fix

--- vsc-extension/src/getImgUrl.ts
+++ vsc-extension/src/getImgUrl.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export async function getImageUrl(userName: string) {
   const endpoint =
     "https://laa1e0jhs0.execute-api.eu-north-1.amazonaws.com/dev/graph/mygraph";
-  const apiKey = "SESNJGvOsE161xSXG5k3A6jNbLwGUiLNanolZbIa"; // ここにAPIキーを設定
+  const apiKey = ""; // ここにAPIキーを設定
 
   // GETリクエストを送信
   return await axios.get(endpoint, {
