67e9d2c71649804b49bc26e9471424c55ebe494f
umaidashi <99778758+umaidashi@users.noreply.github.com>
2023-12-03 01:28:25 +0900

    再確認の画像を表示する領域を作成 (#47)
    * feat: 複数のプログラミング言語に対応
    
    * feat: 画像を表示する領域を作成
    
    * fix
    
    * feat: getMethod api連携
    
    * delete: unused method

--- vsc-extension/src/apiRequests.ts
+++ vsc-extension/src/apiRequests.ts
@@ -1,16 +1,23 @@
-import axios, { AxiosResponse, AxiosError } from 'axios';
+import axios, { AxiosResponse, AxiosError } from "axios";
+import * as dotenv from "dotenv";
+import * as path from "path";
 
-export async function getRequest<T>(userName: string): Promise<AxiosResponse<T> | null> {
-  const url = 'https://01q8r9zev4.execute-api.ap-northeast-1.amazonaws.com/prd/graph/mygraph';
-  const apiKey = "API_KEY";  // ここにAPIキーを設定
+dotenv.config({ path: path.join(__dirname, ".env") });
+const apiKey = process.env.API_KEY; // ここにAPIキーを設定
+
+export async function getRequest<T>(
+  userName: string
+): Promise<AxiosResponse<T> | null> {
+  const url =
+    "https://01q8r9zev4.execute-api.ap-northeast-1.amazonaws.com/prd/graph/mygraph";
   try {
     const response = await axios.get<T>(url, {
       params: {
         userName: userName,
       },
       headers: {
-        'x-api-key': apiKey,
-      }
+        "x-api-key": apiKey,
+      },
     });
     return response;
   } catch (error: any) {
@@ -19,14 +26,20 @@ export async function getRequest<T>(userName: string): Promise<AxiosResponse<T> 
   }
 }
 
-export async function postRequest<T>(data?: Record<string, any>): Promise<AxiosResponse<T> | null> {
-  const url = 'https://01q8r9zev4.execute-api.ap-northeast-1.amazonaws.com/prd/graph/mygraph';
-  const apiKey = "API_KEY";  // ここにAPIキーを設定
+export async function postRequest<T>(
+  data?: Record<string, any>
+): Promise<AxiosResponse<T> | null> {
+  const url =
+    "https://01q8r9zev4.execute-api.ap-northeast-1.amazonaws.com/prd/graph/mygraph";
+  const apiKey = "API_KEY"; // ここにAPIキーを設定
   const headers = {
-    'x-api-key': apiKey,
+    "x-api-key": apiKey,
   };
   try {
-    const response = await axios.post<T>(url, data, { headers, timeout: 20000  });
+    const response = await axios.post<T>(url, data, {
+      headers,
+      timeout: 20000,
+    });
     return response;
   } catch (error: any) {
     handleRequestError(error);
@@ -37,12 +50,15 @@ export async function postRequest<T>(data?: Record<string, any>): Promise<AxiosR
 function handleRequestError(error: AxiosError): void {
   if (error.response) {
     // The request was made and the server responded with a status code
-    console.error(`Request failed with status ${error.response.status}:`, error.response.data);
+    console.error(
+      `Request failed with status ${error.response.status}:`,
+      error.response.data
+    );
   } else if (error.request) {
     // The request was made but no response was received
-    console.error('No response received for the request:', error.request);
+    console.error("No response received for the request:", error.request);
   } else {
     // Something happened in setting up the request that triggered an Error
-    console.error('Error setting up the request:', error.message);
+    console.error("Error setting up the request:", error.message);
   }
 }
